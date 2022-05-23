3b404372334798623b957ede73be6da39c4d764e
Jannik Schwarz-Wolf <jannikschwarz@live.dk>
2022-05-23 11:36:45 +0200

    Add user favorite

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import bodyParser from 'body-parser';
-import { getUser, getFavorites, userLogin, userSignup, addFavorite, initialize } from './routes/user.js';
+import { getUser, getFavorites, userLogin, userSignup, addFavorite, initializeUser } from './routes/user.js';
 import { getComments, addComment } from './routes/comments.js';
 import morgan from 'morgan';
 import cors from 'cors';
+import { initializeApp } from 'firebase/app';
+
+//Instatiation of API components
+function initialize(){
+  const projectId = 'sep6-6733b';
+  const firebaseConfig = {
+      apiKey: process.env.API_KEY,
+      authDomain: `${projectId}.firebaseapp.com`,
+      projectId,
+      storageBucket: `${projectId}.appspot.com`,
+      messagingSenderId: "681323377586",
+      appId: process.env.APP_ID,
+      databaseURL: `https://${projectId}.eur3.firebasedatabase.app`,
+  }
+  const firebaseApp = initializeApp(firebaseConfig);
+  initializeUser(firebaseApp);
+}
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -28,7 +45,9 @@ app.post('/user/:id/favorite',jsonParser,addFavorite);
 app.get('/comments/:movieId',getComments);
 app.post('/comments/:movieId',jsonParser,addComment);
 
+//Instatiate 
 initialize();
+
 // Listen
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
