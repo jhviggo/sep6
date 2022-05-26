88292592552570bd7e68ef1fd7bf29c433c82f00
Jannik Schwarz-Wolf <jannikschwarz@live.dk>
2022-05-26 18:06:34 +0200

    Tests for user done

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,37 +1,46 @@
-import chai, { expect } from "chai";
-import sinon from "sinon";
-import mock from "mock-require";
-
+const chai = require("chai");
+const sinon = require("sinon");
+const mock =  require("mock-require");
+const { expect } = chai;
 chai.should();
 
 describe('User Route API', () => {
-    let getAuthMock;
-    let createUserMock;
-    let signInMock; 
+    let signUpUserMock;
+    let loginMock; 
+    let addUserFavoriteMock;
+    let removeUserFavoriteMock;
+    let getUserFavoritesMock;
     let getUserInfoMock;
 
     beforeEach(() => {
-        getAuthMock = sinon.stub();
-        createUserMock = sinon.stub();
-        signInMock = sinon.stub();
+        signUpUserMock = sinon.stub();
+        loginMock = sinon.stub();
+        addUserFavoriteMock = sinon.stub();
+        removeUserFavoriteMock = sinon.stub();
+        getUserFavoritesMock = sinon.stub();
+        addUserInfoMock = sinon.stub();
         mock('firebase/auth', {
-            'getAuth' : getAuthMock,
-            'createUserWithEmailAndPassword' : createUserMock,
-            'signInWithEmailAndPassword': signInMock
+            'createUserWithEmailAndPassword' : signUpUserMock,
+            'signInWithEmailAndPassword': loginMock
         });
+        getUserInfoMock = sinon.stub().returns('bob');
+        getUserFavoritesMock = sinon.stub().returns('list');
         mock('../lib/repository.js', {
-            'getUserInformation' : getUserInfoMock
+            'getUserInformation' : getUserInfoMock,
+            'signUpUser' : signUpUserMock,
+            'addUserFavorite' : addUserFavoriteMock,
+            'removeUserFavorite' : removeUserFavoriteMock,
+            'getUserFavorites' : getUserFavoritesMock
         });
     });
 
     afterEach(() => {
-        getAuthMock.resetHistory();
-        createUserMock.resetHistory();
-        signInMock.resetHistory();
+        signUpUserMock.resetHistory();
+        loginMock.resetHistory();
     });
 
     it("Should Get user information by ID", async () => {
-        const userMock = mock.reRequire('./routes/user.js');
+        const userMock = mock.reRequire('../routes/user.js');
         const req = {
             params: {
                 id: 123
@@ -42,7 +51,102 @@ describe('User Route API', () => {
             send: sinon.stub()
         };
         await userMock.getUser(req,res);
-        expect(getUserInfoMock).to.have.been.called;
+        sinon.assert.calledWith(getUserInfoMock, 123);
+        sinon.assert.calledWith(res.send,'bob');
+    });
+
+    it("Should Get user favorites by ID", async () => {
+        const userMock = mock.reRequire('../routes/user.js');
+        const req = {
+            params: {
+                id: 123
+            }
+        };
+        const res = {
+            status: () => {},
+            send: sinon.stub()
+        };
+        await userMock.getFavorites(req,res);
+        sinon.assert.calledWith(getUserFavoritesMock, 123);
+        sinon.assert.calledWith(res.send,'list');
+    });
+
+    it("Should remove user favorite by ID", async () => {
+        const userMock = mock.reRequire('../routes/user.js');
+        const req = {
+            params: {
+                id: 123
+            },
+            body: {
+                movieId: 123,
+                imageUrl: 'url',
+                title: 'title'
+            }
+        };
+        const res = {
+            status: () => {},
+            send: sinon.stub()
+        };
+        await userMock.removeFavorite(req,res);
+        sinon.assert.calledWith(removeUserFavoriteMock, 123, 123, 'url', 'title');
+    });
+
+    it("Should add user favorite by ID", async () => {
+        const userMock = mock.reRequire('../routes/user.js');
+        const req = {
+            params: {
+                id: 123
+            },
+            body: {
+                movieId: 123,
+                imageUrl: 'url',
+                title: 'title'
+            }
+        }
+        const res = {
+            status: () => {},
+            send: sinon.stub()
+        };
+        const movie = {
+            movieId: req.body.movieId,
+            imageUrl: req.body.imageUrl,
+            title: req.body.title
+        };
+        await userMock.addFavorite(req,res);
+        sinon.assert.calledWith(addUserFavoriteMock, 123, movie);
+    });
+
+    it("Should login the user via given body information", async () => {
+        const userMock = mock.reRequire('../routes/user.js');
+        const req = {
+            body: {
+                password: 123,
+                email: 'email'
+            }
+        };
+        const res = {
+            status: () => {},
+            send: sinon.stub()
+        };
+        await userMock.userLogin(req,res);
+        sinon.assert.calledWith(loginMock, undefined, 'email', 123);
+    });
+
+    it("Should signup a new user via given body information", async () => {
+        const userMock = mock.reRequire('../routes/user.js');
+        const req = {
+            body: {
+                password: 123,
+                email: 'email',
+                userName: 'bob'
+            }
+        };
+        const res = {
+            status: () => {},
+            send: sinon.stub()
+        };
+        await userMock.userSignup(req,res);
+        sinon.assert.calledWith(signUpUserMock, undefined, 'email', 123);
     });
 
     /**
